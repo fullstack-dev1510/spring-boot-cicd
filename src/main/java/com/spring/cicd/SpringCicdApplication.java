df67c0518e26fb833ff1c0885a681241c286ffac
DELL <DELL@host.docker.internal>
2024-01-08 22:45:47 +0530

    init commit

---
+++ src/main/java/com/spring/cicd/SpringCicdApplication.java
@@ -0,0 +1,20 @@
+package com.spring.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCicdApplication {
+
+	@GetMapping("/")
+    public String displayMessage(){
+        return "Jenkins demo whith Springboot";
+    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdApplication.class, args);
+	}
+
+}
